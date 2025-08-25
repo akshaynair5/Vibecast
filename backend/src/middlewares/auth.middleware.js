9ae077dff8b8435dbd33f12b7a38380ea1d097ae
Akshay Mathilakth Nair <aks7aynair@gmail.com>
2025-08-25 14:51:50 +0530

    Small Changes

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -11,8 +11,8 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
         if(!token){
             throw new ApiError(401,"Unauthorized Request");
         }
-    
-        const decodedToken = await jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
+        console.log("Token:", process.env.ACCESS_TOKEN_SECRET);
+        const decodedToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         const user = await User.findById(decodedToken._id).select("-password -refreshToken");
     
         if(!user){
